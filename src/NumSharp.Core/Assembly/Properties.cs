8411372b0dc1ed8c93ae6fe0e781c9995fce7255
wisblue <wisblue@gmail.com>
2022-07-05 14:20:27 +0800

    successful build on UnitTest
    added PublicKey reference so UnitTest can access internal class members from NumSharp.Core.
    Upgrade to .NET 6.0

--- src/NumSharp.Core/Assembly/Properties.cs
+++ src/NumSharp.Core/Assembly/Properties.cs
@@ -1,6 +1,21 @@
 ﻿using System.Runtime.CompilerServices;
 #if !SIGNING
-[assembly: InternalsVisibleTo("NumSharp.UnitTest")]
-[assembly: InternalsVisibleTo("NumSharp.Benchmark")]
-[assembly: InternalsVisibleTo("TensorFlowNET.UnitTest")]
+[assembly: InternalsVisibleTo("NumSharp.UnitTest,PublicKey=" +
+"00240000048000009400000006020000002400005253413100040000010001004b86c4cb78549b" +
+"34bab61a3b1800e23bfeb5b3ec390074041536a7e3cbd97f5f04cf0f857155a8928eaa29ebfd11" +
+"cfbbad3ba70efea7bda3226c6a8d370a4cd303f714486b6ebc225985a638471e6ef571cc92a461" +
+"3c00b8fa65d61ccee0cbe5f36330c9a01f4183559f1bef24cc2917c6d913e3a541333a1d05d9be" +
+"d22b38cb")]
+[assembly: InternalsVisibleTo("NumSharp.Benchmark,PublicKey=" +
+"00240000048000009400000006020000002400005253413100040000010001004b86c4cb78549b" +
+"34bab61a3b1800e23bfeb5b3ec390074041536a7e3cbd97f5f04cf0f857155a8928eaa29ebfd11" +
+"cfbbad3ba70efea7bda3226c6a8d370a4cd303f714486b6ebc225985a638471e6ef571cc92a461" +
+"3c00b8fa65d61ccee0cbe5f36330c9a01f4183559f1bef24cc2917c6d913e3a541333a1d05d9be" +
+"d22b38cb")]
+[assembly: InternalsVisibleTo("TensorFlowNET.UnitTest,PublicKey=" +
+"00240000048000009400000006020000002400005253413100040000010001004b86c4cb78549b" +
+"34bab61a3b1800e23bfeb5b3ec390074041536a7e3cbd97f5f04cf0f857155a8928eaa29ebfd11" +
+"cfbbad3ba70efea7bda3226c6a8d370a4cd303f714486b6ebc225985a638471e6ef571cc92a461" +
+"3c00b8fa65d61ccee0cbe5f36330c9a01f4183559f1bef24cc2917c6d913e3a541333a1d05d9be" +
+"d22b38cb")]
 #endif
