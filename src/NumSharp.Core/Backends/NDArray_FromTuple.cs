a2aa91d6e2475d96e598a7ec9d850a76f2d378ff
wisblue <wisblue@gmail.com>
2022-07-08 22:43:44 +0800

    add construct NDArray from tuple and np.diffI()

---
+++ src/NumSharp.Core/Backends/NDArray_FromTuple.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using NumSharp.Backends;
+using NumSharp.Backends.Unmanaged;
+using NumSharp.Utilities;
+
+// ReSharper disable once CheckNamespace
+namespace NumSharp
+{
+    public partial class NDArray
+    {
+        public static implicit operator NDArray((double, double, double) tuple) =>
+            new NDArray(new double[,] { { tuple.Item1, tuple.Item2, tuple.Item3 } }, new Shape(new int[] {1,3}));
+
+
+        public static implicit operator NDArray((NDArray, NDArray) tuple) =>
+            np.concatenate((tuple.Item1, tuple.Item2), axis:0);
+
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray) tuple) =>
+            np.concatenate((tuple.Item1, tuple.Item2, tuple.Item3), axis: 0);
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray) tuple) =>
+            np.concatenate((tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4), axis: 0);
+
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray, NDArray) tuple) =>
+            np.concatenate((tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5), axis: 0);
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray) tuple) =>
+            np.concatenate((tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5,
+                            tuple.Item6), axis: 0);
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray, NDArray) tuple) =>
+            np.concatenate((tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5,
+                            tuple.Item6, tuple.Item7), axis: 0);
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray, NDArray, NDArray) tuple) =>
+            np.concatenate((tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5,
+                            tuple.Item6, tuple.Item7, tuple.Item8), axis: 0);
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray, NDArray, NDArray, NDArray) tuple) =>
+            np.concatenate((tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5,
+                            tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9), axis: 0);
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray, NDArray, NDArray, NDArray, NDArray) tuple) =>
+            np.concatenate(new NDArray[] {tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5,
+                            tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10 }, axis: 0);
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray) tuple) =>
+            np.concatenate(new NDArray[] {tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5,
+                            tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10,
+                            tuple.Item11}, axis: 0);
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray, NDArray) tuple) =>
+            np.concatenate(new NDArray[] {tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5,
+                            tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10,
+                            tuple.Item11, tuple.Item12}, axis: 0);
+
+        public static implicit operator NDArray((NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray, NDArray, NDArray, NDArray, NDArray,
+                                                 NDArray, NDArray, NDArray) tuple) =>
+            np.concatenate(new NDArray[] {tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5,
+                            tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10,
+                            tuple.Item11, tuple.Item12, tuple.Item13}, axis: 0);
+
+    }
+}
---
+++ src/NumSharp.Core/Manipulation/np.diff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace NumSharp
+{
+    public static partial class np
+    {
+        public static NDArray diff(NDArray a, int n = 1)
+        {
+            var ret = np.array(a, copy: true);
+
+            for(int i = 1; i < a.shape[0]-n; i++)
+            {
+                ret[i] = a[i + n] - a[i];
+            }
+
+            return ret[":-1"];
+        }
+    }
+}
+
